b2bbc9c9f6a808305f56c6bfdf9a628879ed2484
Bartłomiej Pogodziński <bartek.pogod@gmail.com>
2019-01-30 22:38:28 +0100

    Add Day of the week feature

--- app.js
+++ app.js
@@ -8,7 +8,22 @@ const port=3000;
 app.set('view engine','ejs');
 
 app.get('/',function(req,res){
-  res.sendFile(__dirname+"/index.html");
+  // res.sendFile(__dirname+"/index.html");
+  let dzienTyg=(new Date()).getDay();
+  let strDzien="";
+
+  switch (dzienTyg) {
+    case 0:strDzien="niedziela";  break;
+    case 1:strDzien="poniedziałek";  break;
+    case 2:strDzien="wtorek";  break;
+    case 3:strDzien="środa";  break;
+    case 4:strDzien="czwartek";  break;
+    case 5:strDzien="piątek";  break;
+    case 6:strDzien="sobota"; break;
+    default: strDzien="nowy dzień";
+  }
+
+  res.render("list", {dzien:strDzien});
 });
 
 
